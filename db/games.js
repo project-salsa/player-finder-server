53b7eb5622706cf0ba2c5a77a8b93861911ced3f
Reno DuBois <rdubois96@gmail.com>
2017-10-05 12:14:53 -0500

    Added docstring and module.exports

---
+++ db/games.js
@@ -0,0 +1,24 @@
+const Game = require('./init').Game
+
+/**
+ * createGame - creates a new game in the DB based on parameters
+ * @param name - String - name of the game
+ * @param iconUrl - String -link to the icon used for the game
+ * @param bannerUrl - String - link to the banner used for the game
+ * @param genres - [String] - all of the genres of the game
+ * @returns {Promise} - resolves with data if successful, rejects with err if not
+ */
+function createGame(name, iconUrl, bannerUrl, genres) {
+  return new Promise((resolve, reject) => {
+    Game.create({ name:name, iconUrl:iconUrl, bannerUrl:bannerUrl, genres:genres }, function (err, data) {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(data)
+    })
+  })
+}
+
+module.exports = {
+  createGame: createGame
+}
