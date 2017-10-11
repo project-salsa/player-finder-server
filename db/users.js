a7cb363de7da38befff5b130c33cc92fd448466e
jpadgett7 <johnpadgett7@gmail.com>
2017-10-11 11:14:50 -0500

    Refined error handling and added basic documentation

--- db/users.js
+++ db/users.js
@@ -1,10 +1,18 @@
 const User = require('./init').User
 
+/*
+  getUser - finds a user in the database with given parameter
+  param username - String - the unique name of the user to be pulled from the db
+  returns - Promise - returns user entry upon resolve, rejects with error otherwise
+ */
 function getUser (username) {
   return new Promise((resolve, reject) => {
-    User.findOne({ username: username }, function (err, entry) {
+    User.findOne({ username: username }, (err, entry) => {
       if (err) {
         return reject(err)
+      }
+      else if (typeof(username) === undefined) {
+        return reject(new Error('ERROR: Attempted to pass an undefined object into getUser() function'))
       }
       return resolve(entry)
     })
