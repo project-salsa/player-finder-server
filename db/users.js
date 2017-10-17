d3b359be4361b2c65407784f5b9b300b7ffc7ec1
Dan Castro <dac633@mst.edu>
2017-10-17 15:35:35 -0500

    Added parameter validation for username, password, email

--- db/users.js
+++ db/users.js
@@ -4,11 +4,25 @@ const User = require('./init').User
 
 function createUser (username, password, email) {
   return new Promise((resolve, reject) => {
+    if (username === null) {
+      return reject(new Error('Error: Username field is null'))
+    }
+    if (password === null) {
+      return reject(new Error('Error: Password field is null'))
+    }
+    if (email === null) {
+      return reject(new Error('Error: Email field is null'))
+    }
     const newUser = new User(
       {
         username: username,
         password: password,
-        email: email
+        email: email,
+        subscribedTags: [],
+        notificationTags: [],
+        discordId: '',
+        steamId: '',
+        battleNetId: ''
       })
 
     newUser.save((err, data) => {
