deda7ba7224b7735d2961c3cf5b8f858a9758828
Reno DuBois <renodubois@users.noreply.github.com>
2017-10-20 01:08:56 -0500

    TG-61 Add POST users/

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
+const users = require('../db/users')
 
 // All paths in this file should start with this
 const path = '/users'
@@ -40,11 +41,32 @@ router.get(path + '/', (req, res) => {
  * 500 - Something went wrong
  */
 router.post(path + '/', (req, res) => {
+  const reqData = req.body
+  const response = {
+    success: false,
+    message: ''
+  }
+  const requiredValues = ['username', 'password', 'email']
+  for (const value of requiredValues) {
+    if (typeof reqData[value] === 'undefined') {
+      response.message = 'Required field ' + value + ' is missing or blank'
+      return res.status(400).json(response)
+    }
+  }
+
+  users.createUser(reqData.username, reqData.password, reqData.email).then(() => {
+    response.success = true
+    res.status(201).json(response)
+  }).catch((err) => {
+    response.message = err.message
+    // figure out if it was their fault or ours
+    res.status(500).json(response)
+  })
 })
 
 router.get(path + '/:username', (req, res) => {
   res.send('username is set to ' + req.params.username)
-})
+})  
 
 router.put(path + '/:username', (req, res) => {
   res.send('username is set to ' + req.params.username)
