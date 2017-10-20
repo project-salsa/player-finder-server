3583f40144a299e6313707faf491e0d4fb969df0
Dan Castro <dac633@mst.edu>
2017-10-20 16:51:57 -0500

    Added handler for router.get all requests. Added getRequests mongodb function which returns all requests

--- routes/requests.js
+++ routes/requests.js
@@ -2,11 +2,25 @@
 
 const express = require('express')
 const router = express.Router()
+const getRequests = require('../db/request').getRequests
 
 // All paths in this file should start with this
 const path = '/requests'
 
 router.get(path + '/', (req, res) => {
+  const response = {
+    success: false,
+    message: '',
+    requests: null
+  }
+  getRequests().then((requests) => {
+    response.requests = requests
+    response.success = true
+    return res.status(200).json(response)
+  }).catch((err) => {
+    response.message = err
+    return res.status(500).json(response)
+  })
 })
 
 router.post(path + '/', (req, res) => {
