4f42ddcdafe5a61ff968fcda98b621349a779498
jpadgett7 <johnpadgett7@gmail.com>
2017-10-23 01:08:32 -0500

    Created POST /requests function

--- routes/requests.js
+++ routes/requests.js
@@ -10,6 +10,26 @@ router.get(path + '/', (req, res) => {
 })
 
 router.post(path + '/', (req, res) => {
+  const reqData = req.body
+  const response = {
+    success: false,
+    message: ''
+  }
+  const requiredValues = ['title', 'user', 'game', 'platform', 'tags', 'location', 'maxPlayers']
+  for (const value of requiredValues) {
+    if (typeof reqData[value] === 'undefined') {
+      response.message = 'Required field ' + value + ' is missing or undefined'
+      return res.status(400).json(response)
+    }
+  }
+
+  request.createRequest(reqData.title, reqData.user, reqData.game, reqData.platform, reqData.tags, reqData.location, reqData.maxPlayers).then(() => {
+    response.success = true
+    res.status(201).json(response)
+  }).catch((err) => {
+    response.message = err.message
+    res.status(500).json(response)
+  })
 })
 
 router.get(path + '/:requestId', (req, res) => {
