1c3e70c5e958803de0233b528cf401d404ad7bef
Michael Beaver <michaelbeaver64@gmail.com>
2017-10-23 15:37:08 -0500

    TG-49 changed to use two queries instead of broken populate query

--- db/request.js
+++ db/request.js
@@ -1,19 +1,38 @@
 const Request = require('./init').Request
+const User = require('./init').User
 
+/**
+ * Returns all requests for a specific user
+ * @param username - String - username of user to get requests for
+ * @return {Promise}
+ *    resolve - [Object], or null
+ *        resolves with array of request objects on success, empty array if none
+ *        resolves with null if user does not exist
+ *    reject - Error
+ */
 function getRequestsByUser (username) {
   return new Promise((resolve, reject) => {
-    Request.find().populate({
-      path: 'user',
-      match: { username: username }
-    }).then((data) => {
-      return resolve(data)
-    }, (err) => {
-      return reject(err)
+    User.find({'username': username}).then((err, user) => {
+      if (err !== null && typeof err !== 'undefined') {
+        return reject(err)
+      }
+      if (user.length === 0) {
+        return resolve(null)
+      } else if (user.length > 1) {
+        // TODO Log error that there is more than one user with the same name
+      }
+      Request.find({'user': user[0]._id}).then((err, requests) => {
+        if (err !== null && typeof err !== 'undefined') {
+          return reject(err)
+        }
+        return resolve(requests)
+      }).catch((err) => {
+        return reject(err)
+      })
     }).catch((err) => {
       return reject(err)
     })
   })
-
 }
 
 /**
