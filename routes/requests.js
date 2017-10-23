cb735a7d1076f82a9742eb42f70c1c7910d98e60
Reno DuBois <renodubois@users.noreply.github.com>
2017-10-23 16:37:20 -0500

    Rename db/request to db/requests

--- routes/requests.js
+++ routes/requests.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
-const getRequests = require('../db/request').getRequests
+const getRequests = require('../db/requests').getRequests
 
 // All paths in this file should start with this
 const path = '/requests'
