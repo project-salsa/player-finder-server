8a76ce7213d4e96b9dae5f482e541ffcc1f2e6fa
Michael Beaver <michaelbeaver64@gmail.com>
2017-10-24 15:24:47 -0500

    TG-50 Changed so the function will still work even if there is a compromise in db integrity so as not to interrupt the flow of the app

--- db/request.js
+++ db/request.js
@@ -12,9 +12,8 @@ function getRequestByGame (gameName) {
       console.log(game)
       if (game.length > 1) {
         // TODO Error needs to be logged
-        return reject(new Error('There is a problem with the integrity of the database'))
+        // There is a problem with the integrity of the database
       }
-
       Request.find({'game': game[0]._id}, (err, requests) => {
         if (err !== null && typeof err !== 'undefined') {
           return reject(new Error(err))
