de06f0c7a08de1e8cf120c329edaf288681a8ebc
Michael Beaver <michaelbeaver64@gmail.com>
2017-10-24 15:28:04 -0500

    TG-50 Added doc string and added case if there is no game found

--- db/request.js
+++ db/request.js
@@ -3,6 +3,13 @@
 const Game = require('./init').Game
 const Request = require('./init').Request
 
+/**
+ * Gets a list of requests based on a game name
+ * @param gameName - String - The game name to query by
+ * @return {Promise}
+ *    resolve - [Object] - array of objects returned, [] if none found
+ *        null is returned if game does not exist
+ */
 function getRequestByGame (gameName) {
   return new Promise((resolve, reject) => {
     Game.find({'name': gameName}, (err, game) => {
@@ -13,6 +20,11 @@ function getRequestByGame (gameName) {
       if (game.length > 1) {
         // TODO Error needs to be logged
         // There is a problem with the integrity of the database
+      }
+      if (game.length === 0) {
+        // If there is no game with that name then the request is invalid
+        // so we will resolve with null
+        return resolve(null)
       }
       Request.find({'game': game[0]._id}, (err, requests) => {
         if (err !== null && typeof err !== 'undefined') {
