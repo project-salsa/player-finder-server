2270626670edbb5f8c4531dc821b6ea3a7d29915
Michael Beaver <michaelbeaver64@gmail.com>
2017-12-02 14:27:49 -0600

    Modified login to add user role and id to the JWT

--- auth/login.js
+++ auth/login.js
@@ -8,8 +8,12 @@ const compare = require('./auth').compare
  * @param username - String -  The name of the user who we are trying to log in
  * @param password - String - The password of the user who is trying to log in
  * @return {Promise}
- *  resolve - (Boolean) -
- *      true if user successfully logs in
+ *  resolve - (Boolean || User Info Object) -
+ *      User Object if user successfully logs in
+ *        {
+ *          id: ObjectID,
+ *          role: String
+ *        }
  *      false if it worked, but password is wrong
  *      null if the user doesn't exist
  *  reject - (err) - Rejects if there is an error
@@ -26,7 +30,11 @@ function login (username, password) {
         const iterations = user.iterations
         // Checking to see if given password matches the one in the db
         if (compare(encryptedPassword, password, salt, iterations)) {
-          return resolve(true)
+          const userInfo = {
+            id: user._id,
+            role: user.role
+          }
+          return resolve(userInfo)
         }
         // If compare failed then they must have given the wrong password
         return resolve(false)
